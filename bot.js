6906890e9de3a606ec35ee26abb94f42464baac1
marshmello1 <44791226+marshmello1@users.noreply.github.com>
2019-03-02 01:15:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "+";
 var adminprefix = '#'
 
 
-const developers = ["ID","ID"]
+const developers = ["346343289861046273","346343289861046273"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
